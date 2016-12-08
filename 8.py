338a5f14f10293bcedb500837632fbfad60c1ab3
Petra Vidnerova <petra.vidnerova@gmail.com>
2016-12-08 11:45:17 +0100

    level 8 finished

--- 8.py
+++ 8.py
@@ -1,7 +1,8 @@
 import numpy as np
+import re
 
-#screen = np.zeros((6, 50))
-screen = np.zeros((3, 7))
+screen = np.zeros((6, 50))
+#screen = np.zeros((3, 7))
 
 def rect(A,B):
     global screen 
@@ -19,13 +20,38 @@ def rotate_column(col, B):
     screen = screen.T
     rotate_row(col, B)
     screen = screen.T
-    
-print(screen)
-rect(3, 2)
-print(screen)
-rotate_column(1, 1)
-print(screen)
-rotate_row(0, 4)
-rotate_column(1, 1)
-print()
-print(screen)
+
+def test():    
+    global screen 
+
+    rect(3, 2)
+    rotate_column(1, 1)
+    rotate_row(0, 4)
+    rotate_column(1, 1)
+    print(screen)
+
+
+with open("input8.txt") as file:
+
+    for line in file:
+        line = line.strip()
+        if line.startswith("rect"):
+            A, B = re.search(r'rect (.*)x(.*)', line).groups()
+            rect(int(A), int(B))
+
+        elif line.startswith("rotate row"):
+            row, B = re.search(r'rotate row y=(.*) by (.*)', line).groups()
+            rotate_row(int(row), int(B))
+
+        elif line.startswith("rotate column"):
+            col, B = re.search(r'rotate column x=(.*) by (.*)', line).groups()
+            rotate_column(int(col), int(B))
+
+        else:
+            raise Error("parse error: "+line)
+
+print(sum(sum(screen)))
+
+import matplotlib.pyplot as plot
+plot.imshow(screen)
+plot.show()
