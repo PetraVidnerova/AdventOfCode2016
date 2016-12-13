2fa511b4cfb74207fedf6a772c3b1a4d9d88def9
Petra Vidnerova <petra.vidnerova@gmail.com>
2016-12-13 15:06:29 +0100

    comment

--- day13test.py
+++ day13test.py
@@ -4,6 +4,9 @@ import day13
 class Example(unittest.TestCase): 
 
     def test(self): 
+        """ The lenght of shortest path from [1, 1]
+        to [7, 4] is 11. 
+        """
         len =  day13.path_len((1, 1), (7, 4))
         self.assertEqual(11, len) 
 
