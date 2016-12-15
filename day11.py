a3d31a36a613e891e842dd993aeb4987f728b83e
Petra Vidnerova <petra.vidnerova@gmail.com>
2016-12-15 12:42:53 +0100

    use array as state

--- day11.py
+++ day11.py
@@ -1,136 +1,150 @@
 import copy
-import random
+import itertools 
+import numpy as np
 
-substances = [ 'H', 'L' ]
-objects = [ 'HM', 'LM', 'HG', 'LG' ]
-couples = [ ('HM', 'LM'), ('HM', 'HG'), ('HM', 'LG'),
-            ('LM', 'HG'), ('LM', 'LG'),
-            ('HG', 'LG') ]
+#substances = [ 'H', 'L' ]
+#objects = [ 'HM', 'LM', 'HG', 'LG' ]
+#index = { 'HM': 1, 'LM' : 2, 'HG' : 3, 'LG' : 4 } 
+#couples = [ ('HM', 'LM'), ('HM', 'HG'), ('HM', 'LG'),
+#            ('LM', 'HG'), ('LM', 'LG'),
+#            ('HG', 'LG') ]
+#initial_state = np.array([0, 0, 0, 1, 2], dtype=np.uint8)
+
+
+substances = [ 'S', 'P', 'T', 'R', 'C' ]
+objects = [ 'SM', 'PM', 'TM', 'RM', 'CM', 'SG', 'PG', 'TG', 'RG', 'CG']
+index = { 'SM' : 1, 'PM' : 2, 'TM' : 3, 'RM' : 4, 'CM' : 5, 'SG' : 6, 'PG' : 7, 'TG' : 8, 'RG' : 9, 'CG' : 10 } 
+couples = list(itertools.combinations(objects, 2))
+
+initial_state = np.array([0, 0, 0,  2, 1, 1, 0, 0, 1, 1, 1], dtype=np.uint8)
 
 UP = 1
 DOWN = -1
 
-class Floors:
 
-    def __init__(self):
 
-            self.elevator = 0
-            self.floors = [set(), set(), set(), set()]
-            self.floors[0] = set(['HM', 'LM'])
-            self.floors[1] = set(['HG'])
-            self.floors[2] = set(['LG'])
-            self.floors[3] = set()
+def elevator(state):
+    return state[0] 
 
-    def __str__(self):
-
-        for i in range(4):
-            for o in objects:
-                if o in self.floors[i]:
-                    print(o, end=" ")
-                else:
-                    print(". ",end=" ")
-            print()
-        return ""
+def state_print(state): 
+    for i in range(4):
+        for o in objects:
+            if state[index[o]] == i:
+                print(o, end=" ")
+            else:
+                print(". ",end=" ")
+        print()
             
-        
-    def contains(self, what):
-        return what in self.floors[self.elevator] 
+def contains(state, what):
+    return state[index[what]] == elevator(state) 
 
-    def move(self, direction, what, what2=None):
-        self.floors[self.elevator].remove(what)
-        if what2:
-            self.floors[self.elevator].remove(what2)
-        self.elevator += direction
-        self.floors[self.elevator].add(what)
-        self.floors[self.elevator].add(what2)
 
-    def fires(self):
-        for floor in self.floors:
-            for s in substances:
-                if s+'M' not in floor:
-                    continue # chip not present 
-                if s+'M' in floor and s+'G' in  floor:
-                    continue # chip connected
-                for s2 in substances:
-                    if s2+'G' in floor:
-                        #print("fires")
-                        return True # chip fires
-        return False
-        
-    def finished(self):
-        if 'HM' in self.floors[3] and 'HG' in self.floors[3] and 'LM' in self.floors[3] and 'LG' in self.floors[3]:
-            return True
+def move(state, direction, what, what2=None):
+    if state[index[what]] != elevator(state):
+        return False # move not possible
+    if what2 and state[index[what2]] != elevator(state):
+        return False # move not possible
+
+    if elevator(state) == 0 and direction == DOWN:
+        return False 
+    if elevator(state) == 3 and direction == UP:
+        return False 
+
+    state[0] += direction
+    state[index[what]] += direction 
+    if what2:
+        state[index[what2]] += direction
+    return True
+
+
+def fires(state):
+    gen_start = 1 + (len(state)-1) // 2 
+    floor_safe = [True] * 4 
+    for x in state[gen_start:]:   #go through generators 
+        floor_safe[x] = False 
+    
+    # go through chips 
+    for i, x in enumerate(state[1:gen_start]):
+        if floor_safe[x]: # no generator on floor
+            continue
         else:
-            return False 
+            if x == state[gen_start+i]: # own generator on floor
+                continue
+            else:
+                return True #chip fires 
+    return False 
+        
+
+def finished(state):
+#    return all(state == 3)
+    for x in state[1:]:
+        if x != 3:
+            return False
+    return True 
 
 
 def step(state):
     
     for direction in [ UP, DOWN]:
         #print("direction", direction, state.elevator)
-        if direction == UP and state.elevator == 3:
+        if direction == UP and elevator(state) == 3:
             continue
-        if direction == DOWN and state.elevator == 0:
+        if direction == DOWN and elevator(state) == 0:
             continue
         # move one 
         for o in objects:
-            #print("object", o)
-            if state.contains(o):
+            # if o is on the same floor as elevator
+            if contains(state, o): #
                 next_state = copy.deepcopy(state)
-                #print("move", direction, o)
-                next_state.move(direction, o)
-                if not next_state.fires():
+                move(next_state, direction, o)
+                if not fires(next_state):
                     yield next_state
         #move two
         for o1, o2 in couples:
-            #print("objects", o1, o2)
-            if state.contains(o1) and state.contains(o2):
+            if contains(state, o1) and contains(state, o2):
                 next_state = copy.deepcopy(state)
-                #print("move", direction, o1, o2)
-                next_state.move(direction, o1, o2)
-                if not next_state.fires():
+                move(next_state, direction, o1, o2)
+                if not fires(next_state):
                     yield next_state
 
-states = [(Floors(), 0)]
+
 
 def test():
                         
-    state, steps  = states.pop()
-    print(state) 
+    state   = states.pop()
+    state_print(state) 
     
     for s in step(state):
-        print(s)
-        states.add((s, steps+1))
+        state_print(s)
+        states.append(s)
 
     print("----------")
-    state , steps = states.pop()
-    print(state) 
+    state  = states.pop()
+    state_print(state) 
+
+
+
+
+if __name__ == "__main__": 
+
+    states = [ initial_state ]
+    length = 0
+
+
+    while True:
+        print(length, len(states))
+        new_states = []
+        for state in states: 
+            for newstate in step(state):
+                if finished(newstate):
+                    print(length+1)
+                    quit() 
+                new_states.append(newstate) 
+        states = new_states
+        length += 1
+
+
+
+
 
     
-min = 50 
-while True:
-
-    if len(states) == 0:
-        break
-    
-    #state, steps  = states.pop(random.randint(0, len(states)-1))
-    state, steps = states.pop() 
-    #print(state, steps)
-    #print("--->", path)
-    
-    if min is not None and steps > min:
-        continue
-    
-    for s in step(state):
-        if s.finished():
-            print(steps+1)
-            if min is None or steps+1 < min:
-                min = steps + 1
-        #elif s in path:
-        #    # circle
-        #    continue
-        else:
-            #new_path = path
-            #new_path.add(s)
-            #states.append((s, steps+1, new_path)) 
-            states.append((s, steps+1)) 
