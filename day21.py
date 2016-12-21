dba1967c0608ac78af150c6c57509c6878a1faa2
Petra Vidnerova <petra.vidnerova@gmail.com>
2016-12-21 10:12:25 +0100

    day21 solved

---
+++ day21.py
@@ -0,0 +1,100 @@
+from functools import reduce 
+import re 
+
+def swap(s, x, y):
+    s = list(s)
+    s[x], s[y] = s[y], s[x] 
+    s = reduce(lambda x, y: x + y, s)
+    return s 
+
+def swap_letters(s, a, b):
+    s = s.replace(a, '?')
+    s = s.replace(b, a)
+    s = s.replace('?', b)
+    return s 
+    
+
+def rotate_left(s, x): 
+    x = x % len(s)
+    return s[x:] + s[:x]
+    
+    
+def rotate_right(s, x):
+    x = x % len(s)
+    return s[-x:] + s[:-x]
+
+
+def getindex(s, x):
+    i = 0
+    while i < len(s):
+        if s[i] == x:
+            return i
+        i += 1
+    return -1
+    
+def rotate_position(s, x):
+    index = getindex(s, x)
+    if index >= 4:
+        index += 1
+    index += 1 
+    return rotate_right(s, index)
+
+def reverse(s, x, y): 
+    span = s[x:y+1]
+    return s[:x] + span[::-1] + s[y+1:] 
+
+def move(s, x, y): 
+    letter = s[x]
+    if y > x:
+        return s[:x] + s[x+1:y+1] + letter + s[y+1:]
+    else:
+        return s[:y] + letter + s[y:x] + s[x+1:]
+
+
+if __name__ == "__main__":
+
+    mystring = "abcdefgh"
+
+    with open("input21.txt") as f:
+        for line in f:
+            line = line.strip() 
+
+            if line.startswith("swap position"):
+                x, y = map(int, re.search(r"swap position ([0-9]+) with position ([0-9]+)", line).groups())
+                mystring = swap(mystring, x, y)
+                continue 
+
+            if line.startswith("swap letter"):
+                a, b = re.search(r"swap letter ([a-z]) with letter ([a-z])", line).groups()
+                mystring = swap_letters(mystring, a, b)
+                continue 
+
+            if line.startswith("rotate left"):
+                x = int(re.search(r"rotate left ([0-9]+) step", line).groups()[0])
+                mystring = rotate_left(mystring, x) 
+                continue 
+
+            if line.startswith("rotate right"):
+                x = int(re.search(r"rotate right ([0-9]+) step", line).groups()[0])
+                mystring = rotate_right(mystring, x) 
+                continue 
+
+            if line.startswith("rotate based"):
+                x = re.search(r"rotate based on position of letter ([a-z])", line).groups()[0]
+                mystring = rotate_position(mystring, x)
+                continue 
+
+            if line.startswith("reverse"):
+                x, y  = map(int, re.search(r"reverse positions ([0-9]+) through ([0-9]+)", line).groups())
+                mystring = reverse(mystring, x, y)
+                continue 
+
+            if line.startswith("move"):
+                x, y  = map(int, re.search(r"move position ([0-9]+) to position ([0-9]+)", line).groups())
+                mystring = move(mystring, x, y)
+                continue 
+                
+    print(mystring)
+
+
+
---
+++ day21a.py
@@ -0,0 +1,62 @@
+from functools import reduce 
+import re 
+import day21 
+
+
+def un_rotate_position(s, x):
+    s2 = day21.rotate_left(s, 1)
+    while day21.rotate_position(s2, x) != s:
+        s2 = day21.rotate_left(s2, 1)
+    return s2 
+
+
+if __name__ == "__main__":
+
+    mystring = "fbgdceah"
+
+    lines = [] 
+    with open("input21.txt") as f:
+        for line in f:
+            lines.append(line)
+        
+    for line in lines[::-1]:
+
+        if line.startswith("swap position"):
+            x, y = map(int, re.search(r"swap position ([0-9]+) with position ([0-9]+)", line).groups())
+            mystring = day21.swap(mystring, y, x)
+            continue 
+
+        if line.startswith("swap letter"):
+            a, b = re.search(r"swap letter ([a-z]) with letter ([a-z])", line).groups()
+            mystring = day21.swap_letters(mystring, a, b)
+            continue 
+
+        if line.startswith("rotate left"):
+            x = int(re.search(r"rotate left ([0-9]+) step", line).groups()[0])
+            mystring = day21.rotate_right(mystring, x) 
+            continue 
+
+        if line.startswith("rotate right"):
+            x = int(re.search(r"rotate right ([0-9]+) step", line).groups()[0])
+            mystring = day21.rotate_left(mystring, x) 
+            continue 
+
+        if line.startswith("rotate based"):
+            x = re.search(r"rotate based on position of letter ([a-z])", line).groups()[0]
+            mystring = un_rotate_position(mystring, x)
+            continue 
+
+        if line.startswith("reverse"):
+            x, y  = map(int, re.search(r"reverse positions ([0-9]+) through ([0-9]+)", line).groups())
+            mystring = day21.reverse(mystring, x, y)
+            continue 
+
+        if line.startswith("move"):
+            x, y  = map(int, re.search(r"move position ([0-9]+) to position ([0-9]+)", line).groups())
+            mystring = day21.move(mystring, y, x)
+            continue 
+                
+    print(mystring)
+
+
+
